56efb686cbe49a10bbb623a24cc0943a2ee67ee4
Gabriel Rangel <grangelda@icloud.com>
2021-07-27 21:25:29 -0300

    webpack config; header impl;

---
+++ galeria/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'app.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template:'./src/index.html'}),
+        new MiniCssExtractPlugin({filename: 'style.css'})
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    // inject CSS to page
+                    loader: MiniCssExtractPlugin.loader
+                }, {
+                    // translates CSS into CommonJS modules
+                    loader: 'css-loader'
+                }, {
+                    // Run postcss actions
+                    loader: 'postcss-loader',
+                    options: {
+                        // `postcssOptions` is needed for postcss 8.x;
+                        // if you use postcss 7.x skip the key
+                        postcssOptions: {
+                            // postcss plugins, can be exported to postcss.config.js
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                }, {
+                    // compiles Sass to CSS
+                    loader: 'sass-loader'
+                }]
+            }
+        ]
+    }
+};
